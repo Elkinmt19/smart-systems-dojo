cac7c4e86cb08e06df4ed6f575b699e19edd9595
Elkinmt19 <elkin02011@hotmail.com>
2021-09-16 11:44:31 -0500

    Added the MAX and ARMAX models

--- matlab/class-challenges/box-jenkins/box_jenkins.m
+++ matlab/class-challenges/box-jenkins/box_jenkins.m
@@ -2,8 +2,8 @@
 clear; close all; clc;
 
 % Load the database
-fprintf("Loading database....\n");
-load("mat-files/BASEDATOS_SIMULINK.mat");
+fprintf('Loading database....\n');
+load('mat-files/BASEDATOS_SIMULINK.mat');
 
 yd = ST(:,2); % System response from Simulink
 u = ST(:,1); % System setpoint from Simulink 
@@ -19,14 +19,31 @@ subplot(4,1,2);
 autocorr(yd,size(yd,1) - 1);
 subplot(4,1,3);
 parcorr(yd,100);
-
-% Autoregressive Model
 data = iddata(yd,u,0.01); % Simulation matrix
+
+% Autoregressive Model with exogenous input (ARX)
 yarx = arx(data,[100,1,0]); % [na nb nk] na: Windows of the system response
 ys1 = idsim(u,yarx); % Simulate the arx system
 subplot(4,1,4);
 plot(k,yd,'r',k,ys1,'b');
-fprintf("Done!");
+fprintf('Done!');
+
+% Movile mean model with exogenous input (MAX)
+ymax = armax(data,[0,1,4,0]); % [na nb nc nk] nc: Delays of the error
+ys2 = idsim(u,ymax);
+subplot(4,1,4);
+plot(k,yd,'r',k,ys1,'b');
+fprintf('Done!');
+
+% ARMAX Model
+yarmax = armax(data,[2,1,2,0]);
+ys3 = idsim(u,yarmax);
+subplot(4,1,4);
+plot(k,yd,'r',k,ys1,'b');
+fprintf('Done!');
+
+
+
 
 
 
