ae23b57d799618ef1eed388ab3c4ef26a8a6fb3b
Elkinmt19 <elkin02011@hotmail.com>
2021-09-23 12:45:31 -0500

    Fixes some issues of the box_jenkins.m file

--- matlab/class-challenges/box-jenkins/box_jenkins.m
+++ matlab/class-challenges/box-jenkins/box_jenkins.m
@@ -3,11 +3,7 @@ clear; close all; clc;
 
 % Load the database
 fprintf('Loading database....\n');
-<<<<<<< HEAD
 load('mat-files/dynamic_system_data.mat');
-=======
-load('mat-files/BASEDATOS_SIMULINK.mat');
->>>>>>> cac7c4e86cb08e06df4ed6f575b699e19edd9595
 
 yd = ST(:,2); % System response from Simulink
 u = ST(:,1); % System setpoint from Simulink 
@@ -25,48 +21,26 @@ subplot(4,1,3);
 parcorr(yd,100);
 data = iddata(yd,u,0.01); % Simulation matrix
 
-<<<<<<< HEAD
 % % Autoregressive Model with exogenous input (ARX)
 % yarx = arx(data,[100,1,0]) % [na nb nk] na: Windows of the system response
 % ys1 = idsim(u,yarx); % Simulate the arx system
 % subplot(4,1,4);
 % plot(k,yd,'r',k,ys1,'b');
-% fprintf('Done!');
+% fprintf('Done!\n');
 % 
 % % Movile mean model with exogenous input (MAX)
 % ymax = armax(data,[0,1,4,0]) % [na nb nc nk] nc: Delays of the error
 % ys2 = idsim(u,ymax);
 % subplot(4,1,4);
 % plot(k,yd,'r',k,ys1,'b');
-% fprintf('Done!');
+% fprintf('Done!\n');
 
 % ARMAX Model
-yarmax = armax(data,[2,1,2,0])
+yarmax = armax(data,[5,1,5,0])
 ys3 = idsim(u,yarmax);
 subplot(4,1,4);
 plot(k,yd,'b',k,ys3,'r');
-=======
-% Autoregressive Model with exogenous input (ARX)
-yarx = arx(data,[100,1,0]); % [na nb nk] na: Windows of the system response
-ys1 = idsim(u,yarx); % Simulate the arx system
-subplot(4,1,4);
-plot(k,yd,'r',k,ys1,'b');
-fprintf('Done!');
-
-% Movile mean model with exogenous input (MAX)
-ymax = armax(data,[0,1,4,0]); % [na nb nc nk] nc: Delays of the error
-ys2 = idsim(u,ymax);
-subplot(4,1,4);
-plot(k,yd,'r',k,ys1,'b');
-fprintf('Done!');
-
-% ARMAX Model
-yarmax = armax(data,[2,1,2,0]);
-ys3 = idsim(u,yarmax);
-subplot(4,1,4);
-plot(k,yd,'r',k,ys1,'b');
->>>>>>> cac7c4e86cb08e06df4ed6f575b699e19edd9595
-fprintf('Done!');
+fprintf('Done!\n');
 
 
 
